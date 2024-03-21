4d5d6dd4719cebe46e5cfd6f999308db82ab0439
Mikhail Aristov <12281487+MikhailAristov@users.noreply.github.com>
2024-03-21 15:05:37 +0100

    +refactor gameplay into battle class

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ from gameplay import Battle, Ship
 
 
 def play(attacker: BasicAI, defender: BasicAI):
+    # set up the battle with random ships
     battle = Battle()
     ship_types = list(ships.keys())
     for i in range(3):
@@ -12,26 +13,17 @@ def play(attacker: BasicAI, defender: BasicAI):
         battle.add_attacker(Ship(atk, ships[atk]), i)
         opp = np.random.choice(ship_types)
         battle.add_defender(Ship(opp, ships[opp]), i)
-    print(battle)
+    battle.print_state()
 
-    turn = 1
-    while not battle.victor:
-        next_ship = battle.wait_for_next_ship()
-        print("                             TURN", turn)
-        print(battle)
-        pos = attacker.get_next_target(battle.data)
-        target: Ship = battle.get_ship_at_position(pos)
-        print("{0} fires at {1}!".format(next_ship, target))
-        next_ship.fire(target)
-        if target.is_sunk:
-            battle.remove_ship(target)
-        next_ship.reset_cooldown()
-        print(battle)
-        turn += 1
-
-    print("{0} win the battle!".format(battle.victor))
+    # play out the battle until done
+    while not battle.finished:
+        # advance battle to the next volley
+        data = battle.proceed_to_next_volley()
+        # query the AI what to do next
+        target = attacker.get_next_target(data) if data.active < 3 else defender.get_next_target(data)
+        # fire the volley
+        _ = battle.fire_volley(data.active, target)
 
 
 if __name__ == '__main__':
-    AI = RandomAI()
-    play(AI, AI)
+    play(RandomAI(), RandomAI())
--- gameplay/battle.py
+++ gameplay/battle.py
@@ -14,6 +14,9 @@ class Battle:
     attackers: List[Ship] = [None, None, None]
     defenders: List[Ship] = [None, None, None]
 
+    volley_count: int = 1
+    finished: bool = False
+
     @property
     def all_ships(self) -> List[Ship]:
         return self.attackers + self.defenders
@@ -27,15 +30,45 @@ class Battle:
         ship.hit_points *= randf(.85, 1.)
         self.defenders[pos] = ship
 
-    def __str__(self) -> str:
-        width = 64
-        separator = '~' * width
-        format_str: str = " {1:<" + str(width // 2) + "}{2:>" + str(width // 2) + "} "
-        rows = [separator]
+    def print_state(self):
+        print('~' * 64)
         for i in range(3):
-            rows.append(format_str.format(i + 1, str(self.attackers[i]), str(self.defenders[i])))
-        rows.append(separator)
-        return '\r\n'.join(rows)
+            print(" {1:<31}{2:>31}".format(i + 1, str(self.attackers[i]), str(self.defenders[i])))
+        print('~' * 64)
+
+    def proceed_to_next_volley(self) -> BattleData:
+        print("                            VOLLEY", self.volley_count)
+        next_ship_id, steps = self.next_position_to_fire
+        for s in [s for s in self.all_ships if s is not None]:
+            s.cool(steps)
+        self.print_state()
+        return BattleData([s.data if s else None for s in self.all_ships], next_ship_id)
+
+    def fire_volley(self, attacker_id: int, target_id: int):
+        attacker = self.all_ships[attacker_id]
+        target = self.all_ships[target_id]
+
+        # inflict damage and reset cooldown
+        attacker.fire(target)
+        attacker.reset_cooldown()
+
+        # check if the target is sunk
+        if target.is_sunk:
+            if target_id < 3:
+                self.attackers[target_id] = None
+            else:
+                self.defenders[target_id - 3] = None
+        self.print_state()
+
+        # check if either side has won
+        if target.is_sunk and all(s is None for s in self.defenders):
+            print("The Attackers have won the battle!")
+            self.finished = True
+        elif target.is_sunk and all(s is None for s in self.attackers):
+            print("The Defenders have won the battle!")
+            self.finished = True
+
+        self.volley_count += 1
 
     @property
     def next_position_to_fire(self) -> (int, float):
@@ -46,34 +79,7 @@ class Battle:
                     fastest = i
         return fastest, self.all_ships[fastest].remaining_steps
 
-    def wait_for_next_ship(self) -> Ship:
-        next_ship_id, steps = self.next_position_to_fire
-        result = self.all_ships[next_ship_id]
-        for s in [s for s in self.all_ships if s is not None]:
-            s.cool(steps)
-        return result
-
-    def remove_ship(self, ship: Ship):
-        assert ship is not None
-        for i in range(3):
-            if self.attackers[i] == ship:
-                self.attackers[i] = None
-            elif self.defenders[i] == ship:
-                self.defenders[i] = None
-
-    @property
-    def victor(self) -> str:
-        if all(s is None for s in self.defenders):
-            return "Attackers"
-        elif all(s is None for s in self.attackers):
-            return "Defenders"
-        else:
-            return ""
-
     @property
     def data(self) -> BattleData:
         active, _ = self.next_position_to_fire
         return BattleData([s.data if s else None for s in self.all_ships], active)
-
-    def get_ship_at_position(self, pos: int) -> Ship:
-        return self.all_ships[pos]
